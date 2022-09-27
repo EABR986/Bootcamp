5778c58b1733c0f4431b41de47205a6ebe846642
EABR986 <eduardoblanco198@gmail.comndgit config --global user.email eduardoblanco1987@gmail.com>
2022-09-27 17:30:10 +0200

    feat: iteracion 6

---
+++ Javascript/neoland-basic-algorithms/iteracion6.js
@@ -0,0 +1,28 @@
+// 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+for (let num = 0; num < 10; num++) {
+  console.log(num);
+}
+/*
+1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+cuando el resto del numero dividido entre 2 sea 0.*/
+for (let index = 0; index < 10; index++) {
+  if (index % 2 === 0) {
+    console.log(index);
+  } else {
+    
+  }
+}
+
+/*
+1.3 Crea un bucle para conseguir dormir contando ovejas. 
+Este bucle empieza en 0 y termina en 10. 
+Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle 
+y cambia el mensaje en la última vuelta a 'Dormido!'.*/
+for (let index = 0; index < 10; index++) {
+  if (index < 9) {
+    console.log("Intentando Dormir");
+  } else {
+    console.log("Dormido");
+  }
+}
